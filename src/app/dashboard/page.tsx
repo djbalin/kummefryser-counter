48e8bd954d8f9d6d6561efbb4f9b587845fdb4bc
Jan Balin <jgbalin@gmail.com>
2024-01-19 15:26:56 +0100

    Change title language

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default async function Page() {
   return (
     <CategoriesContextProvider>
       <div className="flex flex-col sm:min-w-[95%] md:min-w-[90%] lg:min-w-[75%] items-center ">
-        <span className="text-3xl pb-10">Hvad har jeg i fryseren :)</span>
+        <span className="text-3xl pb-10">What&apos;s in my freezer :)</span>
         <Suspense fallback={<p>LOADING ITEMS</p>}>
           <ItemsContainer></ItemsContainer>
         </Suspense>
